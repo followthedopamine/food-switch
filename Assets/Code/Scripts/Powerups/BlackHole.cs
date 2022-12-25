d002f76b9ee90d034733ccdd3f590b83afcee056
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-12-25 16:18:08 +1300

    Added star artwork

---
+++ Assets/Code/Scripts/Powerups/BlackHole.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour {
+
+  private void SpawnBlackHole(Vector3Int position) {
+
+  }
+
+  private void SuckTiles(Vector3Int position, GameTile tile) {
+
+  }
+
+  // Shake camera maybe?
+}
