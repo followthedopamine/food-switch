300fe449613053bf27929d22b2f53cbe6efecae8
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-01-15 08:54:00 +1300

    Added placeholders for all UI and created a script to control their displays

--- Assets/Code/Scripts/UIController.cs
+++ Assets/Code/Scripts/UIController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class UIController : MonoBehaviour {
   public static UIController Instance;
-  private GameObject endScreen;
+  private GameObject gameOverScreen;
+  private GameObject gameWonScreen;
+  private GameObject mainMenuScreen;
+  private GameObject levelSelectScreen;
+  private GameObject creditsScreen;
 
   void Awake() {
     if (Instance != null) {
@@ -17,16 +21,63 @@ public class UIController : MonoBehaviour {
   }
 
   void Start() {
-    endScreen = gameObject.transform.Find("Game Over").gameObject;
-    endScreen.SetActive(false);
+    gameOverScreen = gameObject.transform.Find("Game Over").gameObject;
+    gameWonScreen = gameObject.transform.Find("Game Won").gameObject;
+    mainMenuScreen = gameObject.transform.Find("Main Menu").gameObject;
+    levelSelectScreen = gameObject.transform.Find("Level Select").gameObject;
+    creditsScreen = gameObject.transform.Find("Credits Screen").gameObject;
   }
 
-  public void ShowEndScreen() {
+  public void ShowGameOverScreen() {
     // TODO: Need to disable switching while this is up probably
-    endScreen.SetActive(true);
+    DisableAllScreens();
+    gameOverScreen.SetActive(true);
   }
 
-  public void HideEndScreen() {
-    endScreen.SetActive(false);
+  public void HideGameOverScreen() {
+    gameOverScreen.SetActive(false);
+  }
+
+  public void ShowGameWonScreen() {
+    DisableAllScreens();
+    gameWonScreen.SetActive(true);
+  }
+
+  public void HideGameWonScreen() {
+    gameWonScreen.SetActive(false);
+  }
+
+  public void ShowMainMenuScreen() {
+    DisableAllScreens();
+    mainMenuScreen.SetActive(true);
+  }
+
+  public void HideMainMenuScreen() {
+    mainMenuScreen.SetActive(false);
+  }
+
+  public void ShowLevelSelectScreen() {
+    DisableAllScreens();
+    levelSelectScreen.SetActive(true);
+  }
+
+  public void HideLevelSelectScreen() {
+    levelSelectScreen.SetActive(false);
+  }
+
+  public void ShowCreditsScreen() {
+    creditsScreen.SetActive(true);
+  }
+
+  public void HideCreditsScreen() {
+    creditsScreen.SetActive(false);
+  }
+
+  private void DisableAllScreens() {
+    HideGameOverScreen();
+    HideGameWonScreen();
+    HideLevelSelectScreen();
+    HideMainMenuScreen();
+    HideCreditsScreen();
   }
 }
