dd18c8bc9df32d7d880488bb84899bc2143c8075
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2023-01-15 08:06:03 +1300

    Added basic game over UI

---
+++ Assets/Code/Scripts/UIController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+  public static UIController Instance;
+  private GameObject endScreen;
+
+  void Awake() {
+    if (Instance != null) {
+      if (Instance != this) {
+        Destroy(this);
+      }
+    } else {
+      Instance = this;
+    }
+  }
+
+  void Start() {
+    endScreen = gameObject.transform.Find("Game Over").gameObject;
+    endScreen.SetActive(false);
+  }
+
+  public void ShowEndScreen() {
+    endScreen.SetActive(true);
+  }
+
+  public void HideEndScreen() {
+    endScreen.SetActive(false);
+  }
+}
